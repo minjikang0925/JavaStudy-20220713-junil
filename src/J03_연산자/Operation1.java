31218e9e39280555b9a31b623257250fddd899b2
minjikang0925 <rkdajswl62@naver.com>
2022-11-22 18:43:10 +0900

    챕터 3 연산자

---
+++ src/J03_연산자/Operation1.java
@@ -0,0 +1,34 @@
+package J03_연산자;
+
+/*
+ * Ctrl + space -> 자동완성
+ * Ctrl + Alt + 화살표아래 -> 복사
+ */
+ 
+
+public class Operation1 {
+
+	public static void main(String[] args) {
+		
+		int num = 50;
+		
+		System.out.println(++num);
+		System.out.println(num);
+		System.out.println(num++);
+		System.out.println(num);
+		
+		
+		System.out.println(--num);
+		System.out.println(num);
+		System.out.println(num--);
+		System.out.println(num);
+		 
+		/*
+		 * 관계 연산자
+		 * == 두 항이 같으면 참, 아니면 거짓
+		 * != 두 항이 다르면 참, 아니면 거짓
+		 * 
+		 */
+	}
+
+}
